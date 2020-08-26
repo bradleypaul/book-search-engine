d0df3c419a4a20beaac1aa5df023ed539c0cbba7
Paul Bradley <bradleypauld@gmail.com>
2020-08-25 19:12:07 -0500

    mass update

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 const routes = require('./routes');
-
-const app = express();
+const {typeDefs, resolvers} = require('./schemas');
 const PORT = process.env.PORT || 3001;
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
